ed66e645b639083625a6b1ac75d7d9906b528903
Aakash Patel <aakashpatel25274@gmail.com>
2020-04-25 21:46:03 +0530

    MINOR CHANGES

--- src/components/landing/Skills/index.jsx
+++ src/components/landing/Skills/index.jsx
@@ -15,7 +15,7 @@ export const Skills = () => (
       <Details>
         <h1>Hi There!</h1>
         <p>
-          I'm a <strong>Self-thought Full Stack Developer and Blogger</strong>. I started web development in 2018, I love to work with React Js, Gatsby and Node Js.
+          I'm a <strong>Self-taught Full Stack Developer and Blogger</strong>. I started web development in 2018, I love to work with React Js, Gatsby and Node Js.
           and now JAM Stack Development has cought my eyes. &#128526;&#128526;
         </p>
         {/* <Button as={AnchorLink} href="#contact">
